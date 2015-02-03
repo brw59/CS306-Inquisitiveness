ab799e083742a40d670dc11b2affdbe0664bd995
ice123123 <hur13013@byui.edu>
2015-02-03 16:17:39 -0700

    fix tabbing

--- template.cpp
+++ template.cpp
@@ -6,37 +6,37 @@ using namespace std;
 
 void addData(int argc, char file[], vector<int> & storage)
 {
-	if(argc != 1)
-	{
-		cout << "To properly use the program, pass in a file name as a command"
-			  << "line argument" << endl
-		     << "example: ./a.out filename" << endl;
-		exit(0);
-	}
+   if(argc != 1)
+   {
+      cout << "To properly use the program, pass in a file name as a command"
+           << "line argument" << endl
+           << "example: ./a.out filename" << endl;
+      exit(0);
+   }
 
-	int fData;
-	ifstream fin(file);
+   int fData;
+   ifstream fin(file);
 
-	while(fin >> fData)
-	{
-		storage.push_back(fData);
-	}
+   while(fin >> fData)
+   {
+      storage.push_back(fData);
+   }
 }
 
 
 int main(int argc, char * argv[])
 {
-	vector<int> data;
+   vector<int> data;
 
-	addData(argc, argv[1], data);
-
-
+   addData(argc, argv[1], data);
 
 
 
 
 
 
-	return 0;
+
+
+   return 0;
 }
 
